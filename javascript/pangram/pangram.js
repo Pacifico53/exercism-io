fbbc372c794ba6bc0d93d2a6c55d0712e5c076ff
Bernardo Viseu <bernardo2viseu@gmail.com>
2017-11-04 23:53:57 +0000

    pangram done

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,9 +1,13 @@
 function Pangram(input) {
-	this.string = input;
-	isPangram = function(inp) {
-		alphabet = []
-		for () {
-			
+	this.string = input.toLowerCase();
+	let abc = 'abcdefghijklmnopqrstuvwxyz';
+	let r = true;
+	this.isPangram = function(str) {
+		for (var i = 0; i < abc.length && r; i++) {
+			if (!this.string.includes(abc[i])) {r = false;}
 		}
+	return r;
 	}
 }
+
+module.exports = Pangram;
