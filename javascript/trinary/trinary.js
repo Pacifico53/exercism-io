59e4b5203ee19c9a6a5f90bcf6d2d5a3e0204fdb
Bernardo Viseu <bernardo2viseu@gmail.com>
2017-11-18 00:07:42 +0000

    fixes trinary regex for invalid cases

--- javascript/trinary/trinary.js
+++ javascript/trinary/trinary.js
@@ -1,7 +1,7 @@
 function Trinary (input) {
 	this.str = input;
 	this.toDecimal = function() {
-		if (!/[012]/g.test(this.str)) {return 0;}
+		if (!/^[012]+$/g.test(this.str)) {return 0;}
 		let strDecimal = 0;
 		let n = this.str.length-1;
 		for (var i = 0; i < this.str.length; i++) {
