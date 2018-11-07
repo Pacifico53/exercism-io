9e9036af0d56de64376c24985d93a1bd9a778076
BernardoViseu <bernardo2viseu@gmail.com>
2018-11-07 16:41:49 +0000

    adds elixir course

---
+++ elixir/bob/bob.exs
@@ -0,0 +1,24 @@
+defmodule Bob do
+  def hey(input) do
+    str = String.trim(input)
+    cond do
+      is_shouting(str) && is_question(str) -> "Calm down, I know what I'm doing!"
+      is_nothing(str) -> "Fine. Be that way!"
+      is_question(str) -> "Sure."
+      is_shouting(str) -> "Whoa, chill out!"
+      true -> "Whatever."
+    end
+  end
+
+  def is_question(str) do
+    String.last(str) == "?"  
+  end
+
+  def is_nothing(str) do
+    str == ""
+  end
+
+  def is_shouting(str) do
+    str == String.upcase(str) && str =~ ~r([\p{L}]) #This checks if the string has at least one letter character
+  end
+end
